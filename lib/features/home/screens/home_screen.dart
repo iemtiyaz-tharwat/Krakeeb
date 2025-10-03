162228dddcb8dea667325b5021329c850987635e
iemtiyaz.tharwat <iemtiyaztharwat@gmail.com>
2025-10-04 01:21:10 +0300

    edits on home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:krakeeb/core/utils/constants/app_colors.dart';
 import 'package:krakeeb/core/utils/constants/category_values.dart';
 import 'package:krakeeb/core/widgets/custom_app_bar.dart';
+import 'package:krakeeb/features/home/models/waste_item_model.dart';
 import 'package:krakeeb/features/home/widgets/custom_waste_item_fiels.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,26 +14,23 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: AppColors.primary,
       appBar: CustomAppBar(title: "Home"),
       body: Padding(
+        
         padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          itemCount: CategoryValues.wasteItems.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+        
+        child: GridView.count(
+          
+          crossAxisCount: 2,
             crossAxisSpacing: 44,
             mainAxisSpacing: 21,
             childAspectRatio: 0.9,
+            children:
+              List.generate(CategoryValues.wasteItems.length, (index){
+                return
+                CustomWasteItemFiels(item: CategoryValues.wasteItems[index]);
+              })
+            
           ),
-          itemBuilder: (context, index) {
-            final item = CategoryValues.wasteItems[index];
-            return CustomWasteItemFiels(
-              item: item,
-              onTap: () {
-                debugPrint("Tapped on ${item.title}"); //لحد م نعمل الصفحات الي بعدها
-              },
-            );
-          },
         ),
-      ),
-    );
+      );
   }
 }
--- lib/features/home/widgets/custom_waste_item_fiels.dart
+++ lib/features/home/widgets/custom_waste_item_fiels.dart
@@ -2,74 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:krakeeb/core/utils/constants/app_colors.dart';
 import 'package:krakeeb/features/home/models/waste_item_model.dart';
+import 'package:krakeeb/features/home/widgets/custom_elevated_item_botton.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomWasteItemFiels extends StatelessWidget {
   final WasteItem item;
-  final VoidCallback onTap;
 
-  const CustomWasteItemFiels({
-    super.key,
-    required this.item,
-    required this.onTap,
-  });
+  const CustomWasteItemFiels({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: 155,
-        height: 183,
-        decoration: BoxDecoration(
-          color: AppColors.categoryContaineBackGroundColor,
-          borderRadius: BorderRadius.circular(18),
-        ),
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Image.asset(item.imagePath)
-            ),
-            const SizedBox(height: 8),
-
-            
-            Text(
-              item.price,
-              style: const TextStyle(
-                fontSize: 12,
-                color: Color(0XFFA7A7A780),
-                fontWeight: FontWeight.w600,
+    return Container(
+      //width: 155.w,
+     // height: 183.h,
+      decoration: BoxDecoration(
+        color: AppColors.categoryContaineBackGroundColor,
+        borderRadius: BorderRadius.circular(18.r),
+      ),
+      padding: EdgeInsets.all(12),
+      child: Column(
+        children: [
+          Expanded(child: Image.asset(item.imagePath)),
+           SizedBox(height: 8.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              
+              Text(
+                item.price,
+                style: TextStyle(
+                  fontSize: 12.sp,
+                  color: Color(0XFFA7A7A780),
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
+            ],
+          ),
 
-            const SizedBox(height: 4),
-
-            Container(
-              height: 33,
-              width: 155,
-              decoration: BoxDecoration(
-                color: Color(0xffAFDD69),
-                borderRadius: BorderRadius.circular(18),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    item.title,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+           SizedBox(height: 4.h),
+          CustomElevatedItemBotton(item: item),
+        ],
       ),
     );
   }
